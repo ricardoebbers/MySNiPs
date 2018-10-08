417334ad33a8bdfdccf3e74d419acda4b938cabc
João Rafael <jrsc2@cin.ufpe.br>
2018-10-08 05:05:14 -0300

    Definir fluxo de operação

--- MySNiPs/app/gluttony/consume.rb
+++ MySNiPs/app/gluttony/consume.rb
@@ -1,21 +1,40 @@
+require "set"
 require_relative "../snapi/pedia"
-require_relative "./harvest"
+require_relative "./harvester"
+require_relative "./storer"
 
 module Gluttony
   class Consume
     # Controller for SNaPi
     def snpedia
       pedia = SNaPi::Pedia.new
-      harvest = Gluttony::Harvester.new(pedia)
-      store = Gluttony::Storer.new(pedia)
+      @harvest = Gluttony::Harvester.new(pedia)
+      @store = Gluttony::Storer.new(pedia)
 
       # i is just so i don't download every snp when i'm testing
       i = 0
-      while harvest.continue? && i < 5
+      while harvest.continue? && i < 10
         i += 1
+        gather_genes(gather_genos)
+      end
+    end
 
-        harvest.genos.each {|genoid| store.geno(harvest.geno_info(genoid)) }
+    def gather_genos
+      # Sets don't allow repetition and are light enough
+      geneids = Set[]
+      @harvest.genos.each do |genoid|
+        genoinfo = @harvest.geno_info genoid
+        unless genoinfo.nil?
+          @store.geno_info genoinfo
+          geneids.add genoinfo["RSID"]
+        end
+      end
+      geneids
+    end
 
+    def gather_genes(geneids)
+      geneids.each do |geneid|
+        @store.geno_info(@harvest.gene_info(geneid))
       end
     end
   end
