5e1a3268d0f206da03c713e2952c7e2269006ee6
João Rafael <jrsc2@cin.ufpe.br>
2018-10-13 01:51:51 -0300

    Conserta leitura da lista

--- MySNiPs/app/gluttony/storer.rb
+++ MySNiPs/app/gluttony/storer.rb
@@ -3,13 +3,6 @@ module Gluttony
     # Class that will store Genos and Genes in the Database
     # Will treat every Geno individually
     # And, in turn, add its Gene if Harvest returns it
-    attr_reader :genos
-    attr_reader :genes
-    def initialize
-      @genos = []
-      @genes = []
-    end
-
     def close_list(name)
       case name
       when :idswrite then @ids_writefile.close unless @ids_writefile.nil?
@@ -27,7 +20,7 @@ module Gluttony
 
     def read_ids(name)
       @ids_list = File.open(name + ".txt", "r").to_a if @ids_list.nil?
-      line = @ids_list.at(line)
+      line = @ids_list.shift
       line.chomp unless line.nil?
     end
 
