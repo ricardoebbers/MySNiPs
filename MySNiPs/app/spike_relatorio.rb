30218c50f574cee836537306e433e024332813fb
João Rafael <jrsc2@cin.ufpe.br>
2018-10-13 23:46:48 -0300

    Melhora spike de relatório com sort e visual

--- MySNiPs/app/spike_relatorio.rb
+++ MySNiPs/app/spike_relatorio.rb
@@ -1,11 +1,39 @@
 require "set"
 
+class Relation
+  include Comparable
+  attr_accessor :gene
+  attr_accessor :genotype
+  attr_reader :magnitude
+  def initialize(gene, genotype)
+    @gene = gene
+    @genotype = genotype
+    @magnitude = genotype[:magnitude].to_f
+  end
+
+  def <=>(other)
+    other.magnitude <=> @magnitude
+  end
+end
+
+# .
+
+# .
+
+# .
+
 def normal_reversed(str)
   return [str.split("").join(";")] if str == str.reverse
 
   result = str.split("").join(";")
   [result, result.reverse]
 end
+
+# .
+
+# .
+
+# .
 
 gene = []
 File.open("../../Iteracao_2/genes.txt", "r").each do |line|
@@ -31,7 +59,12 @@ geno.each do |g|
 end
 geno = nil
 
-result = Set[]
+# .
+
+# .
+
+# .
+result = SortedSet[]
 File.open("../../Iteracao_2/7829.23andme.6186", "r").each do |line|
   line = line.split("\t")
   gene = ids[line[0]]
@@ -43,28 +76,39 @@ File.open("../../Iteracao_2/7829.23andme.6186", "r").each do |line|
   alleles.each do |p|
     geno = titles["#{gene[:title].downcase}(#{p})"]
     unless geno.nil?
-      result.add [gene, geno]
+      result.add Relation.new gene, geno
       break
     end
   end
 end
 
+# .
+
+# .
+
+# .
 f = File.open("../../Iteracao_2/card_spike.txt", "w")
-result.each do |rest|
-  case rest[1][:repute]
-  when 0 then rep = "Neutral"
-  when 1 then rep = "Good"
-  when 2 then rep = "Bad"
+result.each do |rel|
+  case rel.genotype[:repute]
+  when 0 then
+    rep = "Neutral"
+    appearance = ["- ", "- "]
+  when 1 then
+    rep = "Good"
+    appearance = ["+-", "+-"]
+  when 2 then
+    rep = "Bad"
+    appearance = ["! ", "! "]
   end
 
-  f.puts "_________________________________________________________________"
-  f.puts "\n" + rest[1][:title]
-  f.puts rest[0][:summary] if rest[0].key? :summary
-  f.puts rest[1][:summary] if rest[1].key? :summary
-  f.puts "Magnitude: #{rest[1][:magnitude]}"
+  f.puts "\n" + (appearance[0] * 60)
+  f.puts rel.genotype[:title]
+  f.puts rel.gene[:summary] if rel.gene.key? :summary
+  f.puts rel.genotype[:summary] if rel.genotype.key? :summary
+  f.puts "Magnitude: #{rel.genotype[:magnitude]}"
   f.puts "Repute: #{rep}" unless rep.nil?
-  f.puts "\n" + rest[1][:page_content] unless rest[1][:page_content].empty?
-  f.puts "_________________________________________________________________"
-  f.puts "\n. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
+  f.puts "\n" + rel.genotype[:page_content] unless rel.genotype[:page_content].empty?
+  f.puts appearance[1] * 60
+  f.puts "\n\n\n"
 end
 f.close
