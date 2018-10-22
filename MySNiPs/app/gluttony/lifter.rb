5620f6f432f40bb0f4167025003a88469eba3326
João Rafael <jrsc2@cin.ufpe.br>
2018-10-22 20:32:28 -0300

    Cria estrutura do Lifter que manda dados pro Database

---
+++ MySNiPs/app/gluttony/lifter.rb
@@ -0,0 +1,22 @@
+module Gluttony
+  class Lifter
+    attr_accessor :gene_id
+
+    def initialize
+      @gene_id = {}
+      @count = 0
+    end
+
+    def create_gene(gene)
+      # TO-DO
+    end
+
+    def get_gene_id(title)
+      # TO-DO
+    end
+
+    def create_genotype(geno)
+      # TO-DO
+    end
+  end
+end
