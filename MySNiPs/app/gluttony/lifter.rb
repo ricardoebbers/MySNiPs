eadcbd775c80be62507db4f10e9ca95e43fea181
João Rafael <jrsc2@cin.ufpe.br>
2018-10-22 23:13:13 -0300

    Tenta acessar connection pool 'primary'

--- MySNiPs/app/gluttony/lifter.rb
+++ MySNiPs/app/gluttony/lifter.rb
@@ -1,16 +1,15 @@
 require_relative "../models/application_record"
 require_relative "../models/gene"
 require_relative "../models/genotype"
-class Lifter < ApplicationController
+class Lifter
   attr_accessor :gene_id
 
   def initialize
     @gene_id = {}
-    @count = 0
   end
 
   def create_gene(gene)
-    Gene.new(gene.to_s)
+    g = Gene.create(gene)
   end
 
   def get_gene_id(title)
