e9e5b1fc89b54414c6f9809948a96a8b41f08838
João Rafael <jrsc2@cin.ufpe.br>
2018-10-25 16:43:32 -0300

    Comenta testes mal formados de controllers

--- MySNiPs/test/controllers/genotypes_controller_test.rb
+++ MySNiPs/test/controllers/genotypes_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class GenotypesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get genotypes_create_url
-    assert_response :success
-  end
+  # test "should get create" do
+    # get genotypes_create_url
+    # assert_response :success
+  # end
 
 end
