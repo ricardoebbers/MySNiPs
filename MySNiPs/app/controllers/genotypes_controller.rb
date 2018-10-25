493ace99165c84ca895048f66818539be244e249
João Rafael <jrsc2@cin.ufpe.br>
2018-10-25 03:19:41 -0300

    Começa import from JSON no Genotype controller

--- MySNiPs/app/controllers/genotypes_controller.rb
+++ MySNiPs/app/controllers/genotypes_controller.rb
@@ -1,4 +1,13 @@
-class GenotypesController < ApplicationController
-  def create
+class GenotypesController < GenesController
+  def new
+    @geno = Genotype.new
+  end
+
+  def def index
+    # TO-DO
+  end
+
+  def import_from_file
+    # TO-DO
   end
 end
