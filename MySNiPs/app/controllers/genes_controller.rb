1004bbe0b3c496ffdcd5865dc9e5a823b8acb38a
João Rafael <jrsc2@cin.ufpe.br>
2018-10-25 00:11:55 -0300

    Tenta fazer from_json no Gene Controller

--- MySNiPs/app/controllers/genes_controller.rb
+++ MySNiPs/app/controllers/genes_controller.rb
@@ -1,4 +1,21 @@
 class GenesController < ApplicationController
+  def new
+    @gene = Gene.new
+  end
+
+  def def index
+    # TO-DO
+  end
+
   def create
+    @gene = Gene.new(params[0])
+    redirect_to @gene if @gene.save
+  end
+
+  def import
+    # Please save me, Ebbers
+    gene = Gene.new
+    gene.from_json(params[:gene])
+    gene.save
   end
 end
