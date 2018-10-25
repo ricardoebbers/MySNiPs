5669b73253afcd7da7aaffa6b5867bcddff12a16
João Rafael <jrsc2@cin.ufpe.br>
2018-10-25 01:10:53 -0300

    Termina import from JSON dos Genes

--- MySNiPs/app/controllers/genes_controller.rb
+++ MySNiPs/app/controllers/genes_controller.rb
@@ -1,4 +1,6 @@
 class GenesController < ApplicationController
+  include ActiveModel::Serializers::JSON
+
   def new
     @gene = Gene.new
   end
@@ -7,15 +9,12 @@ class GenesController < ApplicationController
     # TO-DO
   end
 
-  def create
-    @gene = Gene.new(params[0])
-    redirect_to @gene if @gene.save
-  end
-
-  def import
-    # Please save me, Ebbers
-    gene = Gene.new
-    gene.from_json(params[:gene])
-    gene.save
+  def import_from_file
+    path = Rails.root.join("data", "genes.json")
+    file = File.read(path)
+    hash = JSON.parse(file)
+    hash.each do |g|
+      Gene.create(g)
+    end
   end
 end
--- MySNiPs/config/routes.rb
+++ MySNiPs/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'genes/import_from_file'
+
   get 'genes/create'
   get 'genotypes/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'welcome/index'
- 
+
   root 'welcome#index'
 end
