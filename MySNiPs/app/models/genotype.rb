a1d4173c199e55c0e693e9aa54617b46e716aa36
João Rafael <jrsc2@cin.ufpe.br>
2018-10-25 23:03:44 -0300

    Remove S de :gene em Genotype

--- MySNiPs/app/models/genotype.rb
+++ MySNiPs/app/models/genotype.rb
@@ -1,5 +1,5 @@
 class Genotype < ApplicationRecord
-    belongs_to :genes
+    belongs_to :gene
     validates :allele1, length: { is: 1 }
     validates :allele2, length: { is: 1 }
     validates :allele1, :allele2, :title, presence: true
