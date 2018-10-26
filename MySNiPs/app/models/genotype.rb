87686a4a77dd7fe6b3a8e422b614247fa521b878
João Rafael <jrsc2@cin.ufpe.br>
2018-10-26 01:24:48 -0300

    Adicionado validate uniqueness aos models

--- MySNiPs/app/models/genotype.rb
+++ MySNiPs/app/models/genotype.rb
@@ -2,5 +2,6 @@ class Genotype < ApplicationRecord
     belongs_to :gene
     validates :allele1, length: { is: 1 }
     validates :allele2, length: { is: 1 }
-    validates :allele1, :allele2, :title, presence: true
+    validates :allele1, :allele2, presence: true
+    validates :title, presence: true, uniqueness: true
 end
