39e7f4d8ea2f559017228ccadefd1f0d5a09e783
jpmagi10 <jpls@cin.ufpe.br>
2018-10-29 15:46:30 -0300

    Login existente

--- MySNiPs/config/routes.rb
+++ MySNiPs/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/secret'
   get 'genes/import_from_file'
   get 'genotypes/import_from_file'
 
@@ -8,4 +9,18 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   root 'welcome#index'
+
+  # log in page with form:
+	get '/login'     => 'sessions#new'
+	
+	# create (post) action for when log in form is submitted:
+	post '/login'    => 'sessions#create'
+	
+	# delete action to log out:
+  delete '/logout' => 'sessions#destroy'  
+  
+  get 'users/new' => 'users#new', as: :new_user
+	
+	# create (post) action for when sign up form is submitted:
+	post 'users' => 'users#create'
 end
