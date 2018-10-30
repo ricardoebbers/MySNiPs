dcd220ca9079f5243b0f1ebcad8c3e34ca612bb5
Ricardo Ebbers <recl@cin.ufpe.br>
2018-10-30 18:32:26 -0300

    Listagem de genotipos feita

--- MySNiPs/app/models/genotype.rb
+++ MySNiPs/app/models/genotype.rb
@@ -1,7 +1,8 @@
 class Genotype < ApplicationRecord
-    belongs_to :gene
-    validates :allele1, length: { is: 1 }
-    validates :allele2, length: { is: 1 }
-    validates :allele1, :allele2, presence: true
-    validates :title, presence: true, uniqueness: true
+  belongs_to :gene
+  validates :allele1, length: { is: 1 }
+  validates :allele2, length: { is: 1 }
+  validates :allele1, :allele2, presence: true
+  validates :title, presence: true, uniqueness: true
+  paginates_per 50
 end
