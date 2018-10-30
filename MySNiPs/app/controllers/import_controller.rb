a6e6a5b8cac6ea552144e5171cd74f576c3f23fa
João Rafael <jrsc2@cin.ufpe.br>
2018-10-30 01:59:08 -0300

    Finaliza import/from_file com logfile

--- MySNiPs/app/controllers/import_controller.rb
+++ MySNiPs/app/controllers/import_controller.rb
@@ -2,42 +2,71 @@ class ImportController < ApplicationController
   require_relative "genes_controller"
   require_relative "genotypes_controller"
   def from_file
-
     path_genes = Rails.root.join("data", "genes.json")
     path_genos = Rails.root.join("data", "genos.json")
 
-    hash_genes = JSON.parse(File.read(path_genes))
-    hash_genos = JSON.parse(File.read(path_genos))
+    @hash_genes = JSON.parse(File.read(path_genes))
+    @hash_genos = JSON.parse(File.read(path_genos))
 
-    control_genes = GenesController.new
-    control_genos = GenotypesController.new
+    @logfile = File.open(Rails.root.join("data", "logfile.txt"), "w")
 
     last_gene_title = nil
     gene = nil
-    hash_genos.each do |go|
-      gene_title = control_genos.get_gene_from go["title"]
+    gene_saved = false
+    @hash_genos.each do |go|
+      gene_title = get_genetitle_from go["title"]
 
-      if last_gene_title != gene_title
+      if last_gene_title == gene_title && !gene.nil?
+        gene.save unless gene_saved
+        save_genotype(go, gene)
+      else
         gene = Gene.find_by(title: gene_title)
 
-        # If the gene isn't in the database
         if gene.nil?
-          ge = hash_genes.find {|g| g["title"] == gene_title }
-          gene = Gene.new(ge)
+          gene_saved = false
+          gene = read_gene(gene_title)
         end
 
-        unless gene.valid?
-          puts gene.inspect
-          puts gene.errors.messages
+        genotype_saved = save_genotype(go, gene) unless gene.nil?
+        if genotype_saved
+          gene.save
+          gene_saved = true
         else
-          # Genotype
+          gene_saved = false
+          @logfile.puts gene.inspect
+          @logfile.puts "Gene not saved because Genotype was invalid!"
         end
-      else
-
       end
       last_gene_title = gene_title
-
-
     end
+    @logfile.close
+  end
+
+  def save_genotype(geno_hash, gene)
+    geno = gene.genotypes.new(geno_hash)
+    if geno.valid?
+      geno.save
+    else
+      @logfile.puts "\n"
+      @logfile.puts geno.inspect
+      @logfile.puts geno.errors.messages
+    end
+  end
+
+  def get_genetitle_from(genotitle)
+    index = genotitle.index("(") - 1
+    genotitle[0..index]
+  end
+
+  def read_gene(title)
+    ge = @hash_genes.find {|g| g["title"] == title }
+    gene = Gene.new(ge)
+
+    return gene if gene.valid?
+
+    # else
+    @logfile.puts "\n"
+    @logfile.puts gene.inspect
+    @logfile.puts gene.errors.messages
   end
 end
