e72c7f132fda6b4250f4cc5cb0add0f12374b639
João Rafael <jrsc2@cin.ufpe.br>
2018-11-01 20:18:58 -0300

    Pequena alteração para condizer com o RuboCop

--- MySNiPs/app/models/genotype.rb
+++ MySNiPs/app/models/genotype.rb
@@ -2,8 +2,8 @@ class Genotype < ApplicationRecord
   belongs_to :gene
   has_many :cards
   has_many :users, :through => :cards
-  validates :allele1, length: { is: 1 }
-  validates :allele2, length: { is: 1 }
+  validates :allele1, length: {is: 1}
+  validates :allele2, length: {is: 1}
   validates :allele1, :allele2, presence: true
   validates :title, presence: true, uniqueness: true
   paginates_per 50
