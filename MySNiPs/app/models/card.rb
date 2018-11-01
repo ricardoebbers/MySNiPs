525aa7b25fd9629515fbc2fa89749562c0960399
jpmagi10 <jpls@cin.ufpe.br>
2018-10-31 21:56:37 -0300

    relacionamentos de cards, genotypes e users formalizados na model

--- MySNiPs/app/models/card.rb
+++ MySNiPs/app/models/card.rb
@@ -1,2 +1,4 @@
 class Card < ApplicationRecord
+    belongs_to :genotype
+    belongs_to :user
 end
--- MySNiPs/app/models/genotype.rb
+++ MySNiPs/app/models/genotype.rb
@@ -1,5 +1,7 @@
 class Genotype < ApplicationRecord
   belongs_to :gene
+  has_many :cards
+  has_many :users, :through => :cards
   validates :allele1, length: { is: 1 }
   validates :allele2, length: { is: 1 }
   validates :allele1, :allele2, presence: true
