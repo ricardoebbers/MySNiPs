d41f14e3ef4dfc3fb1545946fcc53dcfcd7e2933
jpmagi10 <jpls@cin.ufpe.br>
2018-11-02 14:52:08 -0300

    Ajustes de summary e schema terminados

--- MySNiPs/db/schema.rb
+++ MySNiPs/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_170558) do
+ActiveRecord::Schema.define(version: 2018_11_02_174928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2018_11_02_170558) do
     t.string "iid", limit: 11
     t.string "chromosome", limit: 2
     t.string "position", limit: 10
-    t.text "summary"
+    t.string "summary", limit: 180
     t.string "name", limit: 16
     t.boolean "orientation"
     t.boolean "stabilized"
