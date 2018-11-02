a9036da678b0ae352878263948b740a120161140
João Rafael <jrsc2@cin.ufpe.br>
2018-11-02 03:24:26 -0300

    Faz busca e comparação com o database para os Cards, com flip (704) Resultou em 704 matches, 25 a mais que o spike graças ao flip Guardou os IDs em um arquivo, por enquanto

--- MySNiPs/app/controllers/cards_controller.rb
+++ MySNiPs/app/controllers/cards_controller.rb
@@ -8,13 +8,13 @@ class CardsController < ApplicationController
 
     return if @userid.nil?
 
-    @cards_hash = {}
+    @cards_list = []
     read_file Rails.root.join("data", "genomas", @userid + ".gnm")
   end
 
   def read_file(path)
     # REMOVE
-    i = 0
+    #i = 0
     # REMOVE
     File.open(path, "r").each do |line|
       snp = build_snp line.split("\t")
@@ -23,17 +23,18 @@ class CardsController < ApplicationController
       gene = search_for_gene snp[:title], snp[:chromosome], snp[:position]
       next if gene.nil?
 
-      puts gene.inspect
-
       geno = search_for_genotype snp[:allele1], snp[:allele2], gene
+      next if geno.nil?
 
-      # TO-DO
+      @cards_list << geno[:id]
 
       # REMOVE
-      i += 1
-      break if i == 100
+      #i += 1
+      #break if i == 20
       # REMOVE
     end
+
+    insert_in_db
   end
 
   def build_snp(data)
@@ -55,14 +56,29 @@ class CardsController < ApplicationController
     Gene.find_by(chromosome: chromo, position: posit)
   end
 
-  def search_for_genotype(gene, allele1, allele2)
-    # TO-DO
+  def search_for_genotype(allele1, allele2, gene)
+    # False is minus in SNPedia, thus there's a need to flip
+    if gene[:orientation] == false
+      allele1 = flip allele1
+      allele2 = flip allele2
+    end
+
+    Genotype.find_by(gene_id: gene[:id], allele1: allele1, allele2: allele2)
+  end
+
+  def flip(allele)
+    case allele
+    when "A" then "T"
+    when "T" then "A"
+    when "C" then "G"
+    when "G" then "C"
+    end
   end
 
   def insert_in_db
     log = File.open(Rails.root.join("data", "genomas", @userid + " - log.txt"), "w")
-    @cards_hash.each do |card|
-      log.puts card.inspect
+    @cards_list.each do |card|
+      log.puts card
     end
     log.close
   end
