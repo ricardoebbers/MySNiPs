fbb2295f7495f01c7a0d917897965ae7965b5c51
Ricardo Ebbers <recl@cin.ufpe.br>
2018-11-02 14:35:29 -0300

    Adiciona user no seeds

--- MySNiPs/db/seeds.rb
+++ MySNiPs/db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # x = Gene.create({:title=>"I3002455", :revid=>1195440, :orientation=>false, :stabilized=>false, :iid=>"3002455", :rsid=>"1800386", :position=>6127833, :chromosome=>"12"})
+User.create({:identifier=>"0010000001", :password_digest=>"$2a$10$tUJcvKA.QbKuBSKfno2CtuTjmgLo89/6zdWO3mFMB4LaHBG0Qmz3m"})
