830650c310f34110a17eddc820ab86ebe4650bae
jpmagi10 <jpls@cin.ufpe.br>
2018-11-03 12:11:36 -0300

    Relações model criadas

--- MySNiPs/app/models/user.rb
+++ MySNiPs/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 	has_secure_password
 	has_many :cards
 	has_many :genotypes, :through => :cards
+	belongs_to :roles
 
 	# Verify that identifier field is not blank and that it doesn't already exist in the db (prevents duplicates):
 	validates :identifier, presence: true, uniqueness: true
