57e526443533b15a641d42b208584fe811aca208
Ricardo Ebbers <recl@cin.ufpe.br>
2018-11-03 12:42:35 -0300

    Seta env de testes

--- MySNiPs/features/support/env.rb
+++ MySNiPs/features/support/env.rb
@@ -56,3 +56,18 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
+# Clean the database before and after each scenario
+Before do
+  DatabaseCleaner.start
+end
+
+After do |scenario|
+  DatabaseCleaner.clean
+end
+
+# Register Chrome as the default driver
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+Capybara.javascript_driver = :chrome
