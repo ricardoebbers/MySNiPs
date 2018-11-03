d1b19a2e49130591ba21a228606ad9386e128e53
jpmagi10 <jpls@cin.ufpe.br>
2018-11-03 11:50:47 -0300

    mudanças no schema

--- MySNiPs/db/schema.rb
+++ MySNiPs/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_03_143535) do
+ActiveRecord::Schema.define(version: 2018_11_03_144604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2018_11_03_143535) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "last_login"
+    t.integer "role_id"
     t.index ["identifier"], name: "index_users_on_identifier", unique: true
   end
 
