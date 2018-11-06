94781f970e6206cdd61e1f01e0e64815a653f416
João Rafael <jrsc2@cin.ufpe.br>
2018-11-06 01:35:53 -0200

    Adiciona MasterKey ao web token

--- MySNiPs/lib/json_web_token.rb
+++ MySNiPs/lib/json_web_token.rb
@@ -1,13 +1,13 @@
 class JsonWebToken
   class << self
-    def encode(payload, exp = 720.hours.from_now)
+    def encode(payload, exp = 72.hours.from_now)
       puts "B"
       payload[:exp] = exp.to_i
-      JWT.encode(payload, "masterkey")
+      JWT.encode(payload, Rails.application.credentials.key)
     end
 
     def decode(token)
-      body = JWT.decode(token, "masterkey")[0]
+      body = JWT.decode(token, Rails.application.credentials.key)[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
