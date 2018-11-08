f36f1fa23c7b0cce16d921183e1072d3a25a5804
João Rafael <jrsc2@cin.ufpe.br>
2018-11-07 23:34:01 -0200

    Testes de Genes

---
+++ MySNiPs/spec/controllers/genes_controller_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe GenesController do
+  it "should validate a new Gene" do
+    gene = Gene.new(title: "Valid Gene Title")
+    expect(gene.valid?).to eq true
+  end
+
+  it "should not accept Genes without titles" do
+    gene = Gene.new
+    expect(gene.valid?).to eq false
+  end
+
+  it "should not accept duplicate Genes" do
+    gene1 = Gene.new(title: "Same title")
+    gene1.save
+    gene2 = Gene.new(title: "Same title")
+    notvalid = gene2.valid?
+    gene1.destroy
+    expect(notvalid).to eq false
+  end
+
+  it "should return 'Gene title' when we check the 'Gene title' Gene" do
+    gene = Gene.new(title: "Gene title")
+    puts gene.inspect
+    puts "\n" * 10
+    expect(gene.title).to eq "Gene title"
+  end
+end
