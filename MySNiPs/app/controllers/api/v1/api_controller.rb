0cab73d8cb647131357dde5d22c87ff9f4f0b958
João Rafael <jrsc2@cin.ufpe.br>
2018-11-09 03:07:26 -0200

    Cria namespace Api::V1 e aplica rotas /api/v1/

---
+++ MySNiPs/app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,33 @@
+module Api
+  module V1
+    class ApiController < ApplicationController
+      include Response
+      include ExceptionHandler
+      skip_before_action :verify_authenticity_token
+      before_action :authenticate_request
+
+      attr_reader :current_api_user
+
+      def authority_valid?
+        return false if @current_api_user.nil?
+
+        @role = Role.find(@current_api_user.role_id)
+        return false if @role.nil?
+
+        case @role.role_name
+        when "admin" then true
+        when "laboratorio" then true
+        else false
+        end
+      end
+
+      private
+
+      def authenticate_request
+        @current_api_user = AuthorizeApiRequest.call(request.headers).result
+        puts @current_api_user.inspect
+        render json: { error: 'Not Authorized' }, status: 401 unless @current_api_user
+      end
+    end
+  end
+end
