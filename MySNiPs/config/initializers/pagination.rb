fa553dc7f522c2f99e6a9f789915976531153983
João Rafael <jrsc2@cin.ufpe.br>
2018-11-10 10:26:10 -0200

    Carrega initializers para tentar ajeitar /cards

---
+++ MySNiPs/config/initializers/pagination.rb
@@ -0,0 +1,13 @@
+# These two are needed to make will_paginate work with AA.
+Kaminari.configure do |config|
+  config.page_method_name = :per_page_kaminari
+end
+
+require 'will_paginate/active_record'
+module WillPaginate
+  module ActiveRecord
+    module RelationMethods
+      alias_method :total_count, :count
+    end
+  end
+end
