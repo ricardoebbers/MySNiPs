98b1d5a8fc7a5912eb5b037ca2a653c43bfbf1ed
João Rafael <jrsc2@cin.ufpe.br>
2018-11-10 10:26:19 -0200

    Faz scope from_user para Cards

--- MySNiPs/app/models/card.rb
+++ MySNiPs/app/models/card.rb
@@ -1,7 +1,8 @@
 class Card < ApplicationRecord
-    belongs_to :genotype
-    belongs_to :user
+  belongs_to :genotype
+  belongs_to :user
+  validates :genotype_id, uniqueness: { scope: :user_id }
+  self.per_page = 10
 
-    validates :genotype_id, uniqueness: { scope: :user_id }
-    self.per_page = 10
+  scope :from_user, ->(user_id) { where('user_id = ?', "#{user_id}")}
 end
