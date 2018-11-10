1ed115a58d3eb1528ff2a3268d574cab704f2b7f
João Rafael <jrsc2@cin.ufpe.br>
2018-11-10 12:15:03 -0200

    Faz busca completa nos cards

--- MySNiPs/app/controllers/report_controller.rb
+++ MySNiPs/app/controllers/report_controller.rb
@@ -6,13 +6,17 @@ class ReportController < ApplicationController
     # @cards = Card.where(user_id: @current_user.id).page(params[:page]).per(50) if authorize
     return redirect_to login unless authorize
 
+    @cardsfull = Card.from_user(@current_user.id)
+
+    # filters
+    @cards = @cardsfull
+
     if params.has_key? :search
-      find_summary
-    else
-      @cards = Card.from_user(@current_user.id).paginate(page: params[:page], per_page: 50)
+      find_all
     end
+    @cards = @cards.paginate(page: params[:page], per_page: 50)
   end
-#temporary place for filters?
+
   def find_repute
     @cards = Card.Genotype.reputeIs(params[:reputeIs]) if params[:reputeIs].present?
   end
@@ -21,22 +25,7 @@ class ReportController < ApplicationController
     @cards = Card.Genotype.min_mag(params[:min_mag]).max_mag(params[:max_mag])
   end
 
-  def find_summary
-    @cards = Card .from_user(@current_user.id)
-                  .eager_load(:genotype)
-                  .merge(Genotype.summary_contains(params[:search]))
-                  .paginate(page: params[:page], per_page: 50)
-  end
-
   def find_all
-    @cards = Card .from_user(@current_user.id)
-                  .eager_load(:genotype)
-                  .eager_load(:gene)
-                  .merge(Genotype.title_contains(params[:search]))
-                  .or(merge(Genotype.summary_contains(params[:search]))
-                  .or(merge(Gene.summary_contains(params[:search]))
-                  .or(merge(Gene.chromosome_contains(params[:search]))
-                  .or(Genotype.page_content_contains(params[:page_content])))))
-                  .paginate(page: params[:page], per_page: 50)
+    @cards = @cards.search_for(params[:search])
   end
 end
--- MySNiPs/app/models/card.rb
+++ MySNiPs/app/models/card.rb
@@ -1,8 +1,18 @@
 class Card < ApplicationRecord
   belongs_to :genotype
+  has_one :gene, through: :genotype
   belongs_to :user
-  validates :genotype_id, uniqueness: { scope: :user_id }
+  validates :genotype_id, uniqueness: {scope: :user_id}
   self.per_page = 10
 
-  scope :from_user, ->(user_id) { where('user_id = ?', "#{user_id}")}
+  scope :from_user, ->(user_id) { where("user_id = #{user_id}")}
+  scope :search_for, ->(search) {
+    eager_load(:genotype)
+      .eager_load(:gene)
+      .where("
+              (genotypes.title ILIKE '%#{search}%')
+              OR (genotypes.summary ILIKE '%#{search}%')
+              OR (genotypes.page_content ILIKE '%#{search}%')
+              OR (genes.summary ILIKE '%#{search}%')")
+  }
 end
