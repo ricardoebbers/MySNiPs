1ccde94b23f695f3b79e41018a21339b119cca0b
Ricardo Ebbers <recl@cin.ufpe.br>
2018-11-11 15:27:36 -0300

    rspec api

---
+++ MySNiPs/spec/controllers/api/v1/authentication_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+require 'jwt'
+
+# Documentação da API
+# https://github.com/ricardoebbers/MySNiPs/wiki/Documentação-da-API
+
+describe Api::V1::AuthenticationController, type: :api do
+  it 'should return an API key if the lab credentials is valid' do
+    auth_token = authenticate('001', '654654')
+    expect(auth_token).to have_key(:auth_token)
+  end
+
+  it 'should return an error message if lab credentials are invalid' do
+    auth_token = authenticate('001', 'abcdef')
+    expect(auth_token).to have_key(:error)
+  end
+end
