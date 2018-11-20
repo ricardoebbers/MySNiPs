11d1b1406378f07f086a57260bfca0447ee657f6
João Rafael <jrsc2@cin.ufpe.br>
2018-11-20 15:24:48 -0200

    Remove "id":nil das respostas json da API

--- MySNiPs/app/controllers/concerns/response.rb
+++ MySNiPs/app/controllers/concerns/response.rb
@@ -1,6 +1,6 @@
 module Response
   def json_response(object, status=:ok)
-    result = render json: object, status: status
+    result = render json: object.as_json(except: :id), status: status
     # For easy testing
     puts "Response: " + result
     result
