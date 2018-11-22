16e6680800f31715be2a4b59b2b0ace100ca5668
João Rafael <jrsc2@cin.ufpe.br>
2018-11-22 00:40:06 -0200

    Move /lib para /app/lib e conserta erro em production no database.yml

--- MySNiPs/app/lib/json_web_token.rb
+++ MySNiPs/app/lib/json_web_token.rb
@@ -1,7 +1,6 @@
 class JsonWebToken
   class << self
     def encode(payload, exp = 72.hours.from_now)
-      puts "B"
       payload[:exp] = exp.to_i
       JWT.encode(payload, Rails.application.credentials.key)
     end
