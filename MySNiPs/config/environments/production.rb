231538e287bc6aa3d525d1e9dc4dc9d0b428c410
Ricardo Ebbers <recl@cin.ufpe.br>
2018-11-25 20:26:54 -0300

    Corrige js em prod

--- MySNiPs/config/environments/production.rb
+++ MySNiPs/config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.debug = true
+  #config.assets.debug = true
 end
