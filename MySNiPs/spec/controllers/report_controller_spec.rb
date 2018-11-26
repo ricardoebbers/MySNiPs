687b4a825feb749b63b19dde2e87d65ad46733e5
João Rafael <jrsc2@cin.ufpe.br>
2018-11-26 03:48:47 -0300

    Faz um teste bem básico em Report controller

--- MySNiPs/spec/controllers/report_controller_spec.rb
+++ MySNiPs/spec/controllers/report_controller_spec.rb
@@ -1,5 +1,10 @@
 require "rails_helper"
 
 RSpec.describe ReportController, type: :controller do
-  # ...
+  context "GET index" do
+    it "is a success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
