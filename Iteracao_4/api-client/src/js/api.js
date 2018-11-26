2336e1264c4f233bd16a5c115e0d09074740d69b
Ricardo Ebbers <recl@cin.ufpe.br>
2018-11-26 03:34:42 -0300

    cliente api finalizado

--- Iteracao_4/api-client/src/js/api.js
+++ Iteracao_4/api-client/src/js/api.js
@@ -1,5 +1,5 @@
 
-const url = "http://localhost:3000"
+const url = "http://mysnips.herokuapp.com"
 var authToken;
 var uploadResponse;
 $('.url').text(url);
@@ -8,13 +8,38 @@ $('#btn-auth').click(function (e) {
   let id = $('#lab-id').val();
   let pass = $('#lab-pass').val();
   getAuthToken(id, pass);
-})
+});
 $('#btn-genoma').click(function (e) {
   e.preventDefault();
   let userId = $('#user-id').val();
   let file = document.getElementById('file-genoma').files[0];
-  console.log(file)
   sendGenoma(userId, file);
+});
+$('#btn-genoma-index').click(function (e) {
+  e.preventDefault();
+  getIndexGenoma();
+});
+$('#btn-users-index').click(function (e) {
+  e.preventDefault();
+  getIndexUsers();
+});
+$('#btn-genoma-show').click(function (e) {
+  e.preventDefault();
+  let genomaId = $('#genoma-show-id').val();
+  getGenoma(genomaId);
+});
+$('#btn-user-show').click(function (e) {
+  e.preventDefault();
+  let userId = $('#user-show-id').val();
+  getUser(userId);
+});
+$('#btn-genoma-show-last').click(function (e) {
+  e.preventDefault();
+  getLastGenoma();
+});
+$('#btn-user-show-last').click(function (e) {
+  e.preventDefault();
+  getLastUser();
 });
 
 function getAuthToken(id, pass) {
@@ -31,17 +56,18 @@ function getAuthToken(id, pass) {
       "password": pass
     })
   };
+  console.log(myInit)
   fetch(endpoint, myInit)
-  .then(function (response) {
-    return response.json()
-  })
-  .then(function (data) {
-    authToken = data.auth_token;
-    $(".r1").show();
-    $("#api-key").text(JSON.stringify(data,null,2));
-    $("#auth-token-textarea").val(authToken);
-    M.textareaAutoResize($('#auth-token-textarea'));
-  });
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      authToken = data.auth_token;
+      $(".r1").show();
+      $("#api-key").text(JSON.stringify(data, null, 2));
+      $("#auth-token-textarea").val(authToken);
+      M.textareaAutoResize($('#auth-token-textarea'));
+    });
 }
 
 function sendGenoma(userId, file) {
@@ -72,7 +98,145 @@ function sendGenoma(userId, file) {
       })
       .then(function (data) {
         $(".r2").show();
-        $("#genoma-upload").text(JSON.stringify(data));
+        $("#genoma-upload").text(JSON.stringify(data, null, 2));
       });
   };
+}
+
+function getIndexGenoma() {
+  let endpoint = `${url}/api/v1/genomas`;
+  let httpHeaders = {
+    'Content-Type': 'application/json',
+    'Authorization': authToken
+  };
+  let myHeaders = new Headers(httpHeaders);
+  let myInit = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default',
+  };
+  fetch(endpoint, myInit)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      $(".r3").show();
+      $("#genoma-index").text(JSON.stringify(data, null, 2));
+    });
+}
+
+function getIndexUsers() {
+  let endpoint = `${url}/api/v1/users`;
+  let httpHeaders = {
+    'Content-Type': 'application/json',
+    'Authorization': authToken
+  };
+  let myHeaders = new Headers(httpHeaders);
+  let myInit = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default',
+  };
+  fetch(endpoint, myInit)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      $(".r4").show();
+      $("#users-index").text(JSON.stringify(data, null, 2));
+    });
+}
+
+function getGenoma(genomaId) {
+  let endpoint = `${url}/api/v1/genoma/${genomaId}`;
+  let httpHeaders = {
+    'Content-Type': 'application/json',
+    'Authorization': authToken
+  };
+  let myHeaders = new Headers(httpHeaders);
+  let myInit = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default',
+  };
+  fetch(endpoint, myInit)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      $(".r5").show();
+      $("#genoma-show").text(JSON.stringify(data, null, 2));
+    });
+}
+
+function getUser(userId) {
+  let endpoint = `${url}/api/v1/user/${userId}`;
+  let httpHeaders = {
+    'Content-Type': 'application/json',
+    'Authorization': authToken
+  };
+  let myHeaders = new Headers(httpHeaders);
+  let myInit = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default',
+  };
+  fetch(endpoint, myInit)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      $(".r6").show();
+      $("#user-show").text(JSON.stringify(data, null, 2));
+    });
+}
+
+function getLastGenoma() {
+  let endpoint = `${url}/api/v1/genomas/last`;
+  let httpHeaders = {
+    'Content-Type': 'application/json',
+    'Authorization': authToken
+  };
+  let myHeaders = new Headers(httpHeaders);
+  let myInit = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default',
+  };
+  fetch(endpoint, myInit)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      $(".r7").show();
+      $("#genoma-show-last").text(JSON.stringify(data, null, 2));
+    });
+}
+
+function getLastUser() {
+  let endpoint = `${url}/api/v1/users/last`;
+  let httpHeaders = {
+    'Content-Type': 'application/json',
+    'Authorization': authToken
+  };
+  let myHeaders = new Headers(httpHeaders);
+  let myInit = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default',
+  };
+  fetch(endpoint, myInit)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      $(".r8").show();
+      $("#user-show-last").text(JSON.stringify(data, null, 2));
+    });
 }
